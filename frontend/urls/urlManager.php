a83df0576d680ba8a3a4d91fbe260b467eba3517
asder <web@hildus.com>
2025-10-04 19:58:35 +0300

    restyle

--- frontend/urls/urlManager.php
+++ frontend/urls/urlManager.php
@@ -1,20 +1,5 @@
 <?php
     
-    use core\tools\params\Prefix;
-    use frontend\urls\AnonsRule;
-    use frontend\urls\BloggerRule;
-    use frontend\urls\BookRule;
-    use frontend\urls\BrandRule;
-    use frontend\urls\CategoryRule;
-    use frontend\urls\ChapterRule;
-    use frontend\urls\FaqRule;
-    use frontend\urls\NewsRule;
-    use frontend\urls\PageRule;
-    use frontend\urls\PostRule;
-    use frontend\urls\ProductRule;
-    use frontend\urls\RazdelRule;
-    use frontend\urls\SeoRule;
-    use frontend\urls\ThreadRule;
     use yii\web\UrlManager;
     
     /** @var array $params */
@@ -24,8 +9,8 @@
     
     return [
         'class'           => UrlManager::class,
-        'hostInfo' => Yii::$app->params['frontendHostInfo'],
-        'baseUrl'  => Yii::$app->params['frontendHostInfo'],
+        'hostInfo'        => Yii::$app->params['frontendHostInfo'],
+        'baseUrl'         => Yii::$app->params['frontendHostInfo'],
         'enablePrettyUrl' => true,
         'showScriptName'  => false,
         'cache'           => false,
@@ -35,6 +20,6 @@
             ['pattern' => 'browserconfig', 'route' => 'site/browserconfig', 'suffix' => '.xml'],
             ['pattern' => 'site', 'route' => 'site/webmanifest', 'suffix' => '.webmanifest'],
             ['pattern' => 'robots', 'route' => 'site/robots', 'suffix' => '.txt'],
-            
+        
         ],
     ];
